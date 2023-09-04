61ff92a0e28e24b1e8e952c4121993f4f3b1c1a8
aridamaru <aridamaru7@gmail.com>
2023-09-04 21:16:44 +0400

    Added Equals to Entity

--- Core.Domain/SeedWork/Entity.cs
+++ Core.Domain/SeedWork/Entity.cs
@@ -8,14 +8,8 @@ public abstract class Entity
 
     public virtual int Id
     {
-        get
-        {
-            return _Id;
-        }
-        protected set
-        {
-            _Id = value;
-        }
+        get { return _Id; }
+        protected set { _Id = value; }
     }
 
     private List<INotification> _domainEvents;
@@ -32,10 +26,43 @@ public abstract class Entity
     {
         _domainEvents?.Remove(eventItem);
     }
-    
-    // TODO: AddDomainEvent
-    // TODO: RemoveDomainEvent
-    // TODO: ClearDomainEvents
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents?.Clear();
+    }
+
+    public bool IsTransient()
+    {
+        return Id == default;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return false;
+        }
+
+        if (GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Entity item = (Entity)obj;
+
+        if (item.IsTransient() || IsTransient())
+        {
+            return false;
+        }
+
+        return item.Id == Id;
+    }
     // TODO: Equals
     // TODO: GetHashCode
     // TODO: ==
