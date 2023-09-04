6f88c89de8c125d69654146b9bdf422f0b397682
aridamaru <aridamaru7@gmail.com>
2023-09-04 21:04:45 +0400

    Added DomainEvent to Entity

--- Core.Domain/SeedWork/Entity.cs
+++ Core.Domain/SeedWork/Entity.cs
@@ -2,7 +2,31 @@ namespace Core.Domain.SeedWork;
 
 public abstract class Entity
 {
-    public virtual int Id { get; set; }
+    private int? _requestedHashCode;
+
+    private int _Id;
+
+    public virtual int Id
+    {
+        get
+        {
+            return _Id;
+        }
+        protected set
+        {
+            _Id = value;
+        }
+    }
+
+    private List<INotification> _domainEvents;
+
+    public IReadOnlyCollection<INotification> DomainEvents => _domainEvents?.AsReadOnly();
+
+    public void AddDomainEvent(INotification eventItem)
+    {
+        _domainEvents = _domainEvents ?? new List<INotification>();
+        _domainEvents.Add(eventItem);
+    }
     // TODO: AddDomainEvent
     // TODO: RemoveDomainEvent
     // TODO: ClearDomainEvents
